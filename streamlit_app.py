9526f30ced1d16270181a0359e27cc3035464af6
UserA1984 <andy_menke@yahoo.com>
2026-03-30 08:33:01 -0400

    Update streamlit_app.py
    Fixing API error

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingredients_list:
         st.success(name_on_order + ', your smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+st.text(smoothiefroot_response.json())
